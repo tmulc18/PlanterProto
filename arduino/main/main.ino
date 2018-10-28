78082ef8a73388ed1151f98c9f48edd8049bcb23
Tommy Mulc <tmulc@Tommys-MacBook-Air-2.local>
2018-10-27 23:16:11 -0700

    added EC code arduino

---
+++ arduino/main/main.ino
@@ -0,0 +1,119 @@
+#include <OneWire.h>                                  //for temp
+#include <DallasTemperature.h>                        //for temp
+#include <SoftwareSerial.h>                           //for EC
+
+
+
+// temp
+#define ONE_WIRE_BUS 3  // temperature pin
+
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
+float Celcius=0;
+float Fahrenheit=0;
+String bufferStr="";
+
+// EC
+#define rx 8                                          //EC fake rx (digital)
+#define tx 9                                          //EC fake tx must be pwm
+
+SoftwareSerial myserial(rx, tx);                      //define how the soft serial port is going to work
+
+String inputstring = "";                              //a string to hold incoming data from the PC
+String sensorstring = "";                             //a string to hold the data from the Atlas Scientific product
+boolean input_string_complete = false;                //have we received all the data from the PC
+boolean sensor_string_complete = false;               //have we received all the data from the Atlas Scientific product
+
+void writeString(String stringData) { // Used to serially push out a String with Serial.write()
+
+  for (int i = 0; i < stringData.length(); i++)
+  {
+    Serial.write(stringData[i]);   // Push each char 1 by 1 on each loop pass
+  }
+
+}// end writeString
+
+void setup(void)
+{
+  
+  Serial.begin(9600);
+  sensors.begin();                                    // temp
+
+  myserial.begin(9600);                               // EC. set baud rate for the software serial port to 9600
+  inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
+  sensorstring.reserve(30);                           //set aside some bytes for receiving data from Atlas Scientific product
+}
+
+void loop(void)
+{ 
+  //TEMP
+  sensors.requestTemperatures(); 
+  Celcius=sensors.getTempCByIndex(0);
+  Fahrenheit=sensors.toFahrenheit(Celcius);
+  bufferStr="C "+String(Celcius)+" F  "+String(Fahrenheit)+"\n";
+  writeString(bufferStr);
+  delay(1000);
+
+  //check serial in
+  //Serial.write("Hello Pi, from Arduino\n");
+  if (Serial.available() > 0)
+  {
+    bufferStr=Serial.readString();
+    Serial.println(bufferStr);
+  }
+
+  // EC
+  if (myserial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
+    char inchar = (char)myserial.read();              //get the char we just received
+    sensorstring += inchar;                           //add the char to the var called sensorstring
+    if (inchar == '\r') {                             //if the incoming character is a <CR>
+      sensor_string_complete = true;                  //set the flag
+    }
+  }
+
+  if (sensor_string_complete == true) {               //if a string from the Atlas Scientific product has been received in its entirety
+    if (isdigit(sensorstring[0]) == false) {          //if the first character in the string is a digit
+      Serial.println(sensorstring);                   //send that string to the PC's serial monitor
+    }
+    else                                              //if the first character in the string is NOT a digit
+    {
+      print_EC_data();                                //then call this function 
+    }
+    sensorstring = "";                                //clear the string
+    sensor_string_complete = false;                   //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
+  }
+
+}
+
+void print_EC_data(void) {                            //this function will parse the string  
+
+  char sensorstring_array[30];                        //we make a char array
+  char *EC;                                           //char pointer used in string parsing
+  char *TDS;                                          //char pointer used in string parsing
+  char *SAL;                                          //char pointer used in string parsing
+  char *GRAV;                                         //char pointer used in string parsing
+  float f_ec;                                         //used to hold a floating point number that is the EC
+  
+  sensorstring.toCharArray(sensorstring_array, 30);   //convert the string to a char array 
+  EC = strtok(sensorstring_array, ",");               //let's pars the array at each comma
+  TDS = strtok(NULL, ",");                              //let's pars the array at each comma
+  SAL = strtok(NULL, ",");                              //let's pars the array at each comma
+  GRAV = strtok(NULL, ",");                           //let's pars the array at each comma
+
+  Serial.print("EC:");                                //we now print each value we parsed separately
+  Serial.println(EC);                                 //this is the EC value
+
+  Serial.print("TDS:");                               //we now print each value we parsed separately
+  Serial.println(TDS);                                //this is the TDS value
+
+  Serial.print("SAL:");                               //we now print each value we parsed separately
+  Serial.println(SAL);                                //this is the salinity value
+
+  Serial.print("GRAV:");                              //we now print each value we parsed separately
+  Serial.println(GRAV);                               //this is the specific gravity
+  Serial.println();                                   //this just makes the output easier to read
+  
+//f_ec= atof(EC);                                     //uncomment this line to convert the char to a float
+}
