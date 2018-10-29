98cae29bdeea99ddae6d960e45c5150093439892
Tommy Mulc <tmulc@Tommys-MacBook-Air-2.local>
2018-10-28 17:27:10 -0700

    added duration and interval to timelapse

--- timelapse.py
+++ timelapse.py
@@ -1,16 +1,31 @@
+import argparse
 import time
 from picamera import PiCamera
 
 
-def main(fname='pic_',interval=5):
-	"""Takes photo every inteval minutes."""
+def main(interval, duration, fname='pic_'):
+	"""Takes photo every inteval minutes 
+	for duration many hours."""
 
 	camera = PiCamera()
 	camera.resolution = (1024, 768)
-	camera.capture(fname+'.jpg')
-	time.sleep(interval*1) #wait for the next photo
 
-	
+	num_photos = (duration*60.0)/interval
+	for i in range(num_photos):
+		camera.capture(fname+str(i)+'.jpg')
+		time.sleep(interval*60) #wait for the next photo
+
 
 if __name__ == "__main__":
-	main()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-interval', default = 5
+						help='number of minutes between photos')
+	parser.add_argument('-duration', default = 24
+						help='number of hours of timelapse')
+
+	param_dict = vars(parser.parse_args())
+
+	interval = param_dict['interval']
+	duration = param_dict['duration']
+
+	main(interval, duration)
