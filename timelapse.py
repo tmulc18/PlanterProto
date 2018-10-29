b8ac09f913955abc27f720ad3043a829cf5347ae
Tommy Mulc <tmulc@Tommys-MacBook-Air-2.local>
2018-10-28 17:16:09 -0700

    removed scipy import

--- timelapse.py
+++ timelapse.py
@@ -1,14 +1,11 @@
 import time
 from picamera import PiCamera
-from picamera.array import PiRGBArray
-import scipy.misc
-
 
 
 def main(fname='pic_',interval=5):
 	"""Takes photo every inteval minutes."""
 
-	camera = piCamera()
+	camera = PiCamera()
 	camera.resolution = (1024, 768)
 	camera.capture(fname+'.jpg')
 	time.sleep(interval*1) #wait for the next photo
