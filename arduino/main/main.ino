c45dd5e35a8f4508593b9cea451776b181249695
Tommy Mulc <tmulc@Tommys-MacBook-Air-2.local>
2018-11-07 23:43:19 -0800

    added arduino code for temp and humidity sensor

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -14,6 +14,7 @@ DallasTemperature sensors(&oneWire);
 float Celcius=0;
 float Fahrenheit=0;
 String bufferStr="";
+String ecCommand = "";
 
 // EC
 #define rx 8                                          //EC fake rx (digital)
@@ -35,6 +36,19 @@ void writeString(String stringData) { // Used to serially push out a String with
 
 }// end writeString
 
+
+// Air temp + humidty
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#define DHTTYPE           DHT22     // DHT 22 (AM2302)
+#define DHTPIN            10         // Pin which is connected to the DHT sensor.
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
+// end Air temp + humidity
+
 void setup(void)
 {
   
@@ -44,10 +58,25 @@ void setup(void)
   myserial.begin(9600);                               // EC. set baud rate for the software serial port to 9600
   inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
   sensorstring.reserve(30);                           //set aside some bytes for receiving data from Atlas Scientific product
+
+  // air temp + humidity
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
 }
 
 void loop(void)
 { 
+  //check serial in
+  //Serial.write("Hello Pi, from Arduino\n");
+  if (Serial.available() > 0)
+  {
+    bufferStr=Serial.readString();
+    Serial.println(bufferStr);
+  }
+  
   //TEMP
   sensors.requestTemperatures(); 
   Celcius=sensors.getTempCByIndex(0);
@@ -56,36 +85,65 @@ void loop(void)
   writeString(bufferStr);
   delay(1000);
 
-  //check serial in
-  //Serial.write("Hello Pi, from Arduino\n");
-  if (Serial.available() > 0)
-  {
-    bufferStr=Serial.readString();
-    Serial.println(bufferStr);
-  }
-
   // EC
-  if (myserial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
-    char inchar = (char)myserial.read();              //get the char we just received
-    sensorstring += inchar;                           //add the char to the var called sensorstring
-    if (inchar == '\r') {                             //if the incoming character is a <CR>
-      sensor_string_complete = true;                  //set the flag
+  //send command to get reading.  use most recent temperature reading.
+  ecCommand = "RT,"+String(Celcius);
+  myserial.print(ecCommand);
+  myserial.print('\r');
+  delay(1000);
+
+
+  // get output from EC device
+  for (int i = 0; i < 1000; i++){                     // outer to loop to keep checking for data from EC
+    if (myserial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
+      char inchar = (char)myserial.read();              //get the char we just received
+      sensorstring += inchar;                           //add the char to the var called sensorstring
+      if (inchar == '\r') {                             //if the incoming character is a <CR>
+        sensor_string_complete = true;                  //set the flag
+      }
+    }
+  
+    if (sensor_string_complete == true) {               //if a string from the Atlas Scientific product has been received in its entirety
+      if (isdigit(sensorstring[0]) == false) {          //if the first character in the string is a digit
+        Serial.println(sensorstring);                   //send that string to the PC's serial monitor
+      }
+      else                                              //if the first character in the string is NOT a digit
+      {
+        print_EC_data();                                //then call this function 
+      }
+      sensorstring = "";                                //clear the string
+      sensor_string_complete = false;                   //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
     }
   }
 
-  if (sensor_string_complete == true) {               //if a string from the Atlas Scientific product has been received in its entirety
-    if (isdigit(sensorstring[0]) == false) {          //if the first character in the string is a digit
-      Serial.println(sensorstring);                   //send that string to the PC's serial monitor
-    }
-    else                                              //if the first character in the string is NOT a digit
-    {
-      print_EC_data();                                //then call this function 
-    }
-    sensorstring = "";                                //clear the string
-    sensor_string_complete = false;                   //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
+  // Temp and Humidity
+  // Delay between measurements.
+  delay(delayMS);
+  // Get temperature event and print its value.
+  sensors_event_t event;  
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println("Error reading temperature!");
+  }
+  else {
+    Serial.print("Temperature: ");
+    Serial.print(event.temperature);
+    Serial.println(" *C");
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println("Error reading humidity!");
+  }
+  else {
+    Serial.print("Humidity: ");
+    Serial.print(event.relative_humidity);
+    Serial.println("%");
   }
 
-}
+
+}// end main
+
 
 void print_EC_data(void) {                            //this function will parse the string  
 
